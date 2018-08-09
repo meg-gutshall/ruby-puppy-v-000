7fcccde47617c208be7eac4c85abb4fa2d43ec5e
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-09 20:42:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog
   attr_accessor :name
 
-  @@all = []
+  @@all = []    # Track each instance of Dog
 
   def initialize(name)
     @name = name
     @@all << self
   end
 
+  # Get every dog's name
   def self.all
     @@all.each {|dog| puts dog.name}
   end
